0fd98ededacbb49fd8aba16e5e8bbd8a83ed7842
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-06-13 14:13:59 -0400

    configuracion de las rutas en el app-routing.module configuracion del routerLink desde los componentes html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'layout', pathMatch: 'full' }, //cuando la ruta de la direccion es vacia que redireccione al componente layout
+  { path: 'layout', component: LayoutComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: 'layout', pathMatch: 'full' }// si el usuario introduce una URL incorrecta lo redireccionas al layout
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
